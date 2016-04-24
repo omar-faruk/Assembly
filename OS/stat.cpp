1b8873921f00a863564da410f3f6057766ca0b61
Omar Faruk <munna93_bd@yahoo.com>
2016-04-24 20:54:49 +0600

    Create stat.cpp

---
+++ OS/stat.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <time.h>
+int main(int argc, char*argv[])
+{
+struct stat file;
+int n;
+if (argc != 2)
+{
+printf("Usage: ./a.out <filename>\n");
+exit(-1);
+}
+if ((n = stat(argv[1], &file)) == -1)
+{
+perror(argv[1]);
+exit(-1);
+}
+printf("User id : %d\n", file.st_uid);
+printf("Group id : %d\n", file.st_gid);
+printf("Block size : %d\n", file.st_blksize);
+printf("Blocks allocated : %d\n", file.st_blocks);
+printf("Inode no. : %d\n", file.st_ino);
+printf("Last accessed : %s", ctime(&(file.st_atime)));
+printf("Last modified : %s", ctime(&(file.st_mtime)));
+printf("File size : %d bytes\n", file.st_size);
+printf("No. of links : %d\n", file.st_nlink);
+printf("Permissions : ");
+printf( (S_ISDIR(file.st_mode)) ? "d" : "-\n");
+printf( (file.st_mode & S_IRUSR) ? "r" : "-\n");
+printf( (file.st_mode & S_IWUSR) ? "w" : "-\n");
+printf( (file.st_mode & S_IXUSR) ? "x" : "-\n");
+printf( (file.st_mode & S_IRGRP) ? "r" : "-\n");
+printf( (file.st_mode & S_IWGRP) ? "w" : "-\n");
+printf( (file.st_mode & S_IXGRP) ? "x" : "-\n");
+printf( (file.st_mode & S_IROTH) ? "r" : "-\n");
+printf( (file.st_mode & S_IWOTH) ? "w" : "-\n");
+printf( (file.st_mode & S_IXOTH) ? "x" : "-\n");
+printf("\n");
+if(file.st_mode & S_IFREG)
+printf("File type : Regular\n");
+if(file.st_mode & S_IFDIR)
+printf("File type : Directory\n");
+
+
+
+
+}
